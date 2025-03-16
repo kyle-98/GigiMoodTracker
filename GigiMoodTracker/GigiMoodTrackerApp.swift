736494b7a2319ae8b248967231d6c447735796da
kyle <kforker1@gmail.com>
2025-03-16 17:03:56 -0400

    Initial Commit

---
+++ GigiMoodTracker/GigiMoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GigiMoodTrackerApp.swift
+//  GigiMoodTracker
+//
+//  Created by Kyle Forker on 3/16/25.
+//
+
+import SwiftUI
+
+@main
+struct GigiMoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
